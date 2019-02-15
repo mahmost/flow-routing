41fc78b126eaa61d2c01f7eeb945f90167c7840b
Mahmoud Mostafa <mah@moud.info>
2019-02-15 19:08:27 +0200

    Initial conversion to vue-router

--- package.js
+++ package.js
@@ -7,34 +7,40 @@
 'use strict';
 
 Package.describe({
-  name: 'useraccounts:flow-routing',
-  summary: 'UserAccounts package providing routes configuration capability via kadira:flow-router.',
+  name: 'mahmost:useraccounts-vue-router',
+  summary: 'UserAccounts package providing routes configuration capability via vue-router.',
   version: '1.14.2',
-  git: 'https://github.com/meteor-useraccounts/flow-routing.git',
+  git: '',
+});
+
+Npm.depends({
+    "vue-router": '2.7.0'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0.3');
+  api.versionsFrom('METEOR@1.5.1');
 
   api.use([
     'check',
-    'kadira:flow-router',
+    'ecmascript',
+    'vuejs:blaze-integration',
+    //'kadira:flow-router',
     'underscore',
     'useraccounts:core',
     'modules'
   ], ['client', 'server']);
 
   api.imply([
-    'kadira:flow-router@2.10.1',
+    //'kadira:flow-router@2.10.1',
     'useraccounts:core@1.14.2',
   ], ['client', 'server']);
 
-  api.use([
-     'react@0.14.1_1',
-     'kadira:blaze-layout@2.3.0',
-     'kadira:react-layout@1.5.2',
-     'gwendall:blaze-to-react@0.1.2'
-  ], ['client', 'server'], { weak: true });
+//  api.use([
+//     'react@0.14.1_1',
+//     'kadira:blaze-layout@2.3.0',
+//     'kadira:react-layout@1.5.2',
+//     'gwendall:blaze-to-react@0.1.2'
+//  ], ['client', 'server'], { weak: true });
 
   api.addFiles([
     'lib/core.js',
@@ -42,6 +48,6 @@ Package.onUse(function(api) {
 
   api.addFiles([
     'lib/client/client.js',
-    'lib/client/templates_helpers/at_input.js',
+//    'lib/client/templates_helpers/at_input.js',
   ], ['client']);
 });
